8edd372f176c493bad3a1bfadf1624f868338c47
Sayyad Ali Khan <sayyadali40@gmail.com>
2018-08-06 12:00:54 +0530

    Updating just for refrence purpose

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,14 +1,14 @@
 //add this file to .gitignore
-
+//this file should not be availabe to public
 module.exports = {
   google:{
-    clientID: '518492979193-e7ij0a02mabre5o69q3e7ott89qia9u6.apps.googleusercontent.com',
-    clientSecret: 't5Iv_OYZzl7V1nNfXgn1kAVf'
+    clientID: 'nfkdskajfkafj', //clientId & clientSecret has been changed for security purpose
+    clientSecret: 'fsfaff' 
   },
   mongo:{
     dbUri: 'mongodb://127.0.0.1:27017/worldcup'
   },
   session:{
-    secret:'randomsecretprojectfifaschedule'
+    secret:'sfddffrfbbrn4nr' //secret has been chnaged for security purpsoe
   }
 }
